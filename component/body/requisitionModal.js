cdece83cc478288d243818e356fb2cc0a4a1cbb8
wabloan <wabloan@wabloan.com>
2022-09-02 13:44:24 +0100

    ok

--- component/body/requisitionModal.js
+++ component/body/requisitionModal.js
@@ -1,5 +1,6 @@
 const newQuotationEdit = [];
 const loadEdit = [];
+let currencyFetch
 function requisitionModal(supplierid,orderid,rowid){
    
     document.querySelector('.modalClass').classList.add('modalClassCustom');
@@ -81,8 +82,8 @@ function requisitionModal(supplierid,orderid,rowid){
 
                                </table>
                                <div class="discountTotal">
-                                    <div>Discount(%)</div> 
-                                    <div id="discount">0%</div>
+                                    <div>Discount(%) :- </div> 
+                                    <div id=""><input type="number" id="discount" min="0" class="form-control discountWidth" value="0%"/></div>
                                 </div>
                                 <div class="discountTotal">
                                     <div>Grand Total</div> 
@@ -104,6 +105,8 @@ function requisitionModal(supplierid,orderid,rowid){
         close();
     
         fetchEditx(supplierid,orderid,rowid);
+        modifyPrice();
+        modifyDiscount()
         
         
 }
@@ -154,32 +157,112 @@ function fetchEditx(supplierid,orderid,id){
                             <tr>
                                 <td>${index + 1}</td>
                                 <td>${d.description}</td>
-                                <td><input type="number" min="0" value= ${d.quantity} class="form-control"/></td>
-                                <td><input type="number" min="0" value=${d.price} class="form-control" /></td>
-                                <td><input type="number" disabled value= ${d.total} class="form-control" /></td>
+                                <td><input type="number" min="0" value= ${d.quantity} class="form-control quantity"/></td>
+                                <td><input type="number" min="0" value=${d.price} class="form-control price"  /></td>
+                                <td><input type="number" disabled value= ${d.total} class="form-control " /></td>
                             </tr>
                             `
             })
-            document.querySelector('tbody').innerHTML=dataset
-            document.getElementById('addr').innerHTML = address
-            document.getElementById('suppliername').innerHTML = supplier_name
-            document.getElementById('suppliername_2').value = supplier_name
-            document.getElementById('discount').innerHTML = discount
-            document.getElementById('phonenumber').innerHTML = phonenumber
-            document.getElementById('projectname').value = projectname
-            document.getElementById('currency').value = currency
-            document.getElementById('date').value = mydate
-            document.getElementById('dateofsending').value = dateofsending
-            document.getElementById('ordertitle').value = orderTitle
-            document.getElementById('orderRef').value = orderRef
-            document.getElementById('fileRef').value = fileRef
+            document.querySelector('tbody').innerHTML=dataset;
+            document.getElementById('addr').innerHTML = address;
+            document.getElementById('suppliername').innerHTML = supplier_name;
+            document.getElementById('suppliername_2').value = supplier_name;
+            document.getElementById('discount').value = discount;
+            document.getElementById('phonenumber').innerHTML = phonenumber;
+            document.getElementById('projectname').value = projectname;
+            document.getElementById('currency').value = currency;
+            currencyFetch = currency;
+            document.getElementById('date').value = mydate;
+            document.getElementById('dateofsending').value = dateofsending;
+            document.getElementById('ordertitle').value = orderTitle;
+            document.getElementById('orderRef').value = orderRef;
+            document.getElementById('fileRef').value = fileRef;
             let dis = parseFloat(discount) /100;
             let count = dis * sum;
-            document.getElementById('grandtotal').innerHTML = ` (${currency}) ` + numberWithCommas(parseFloat(sum - count))  
+            let getcurrency = currencySelect(currency);
+            document.getElementById('grandtotal').innerHTML = ` (${getcurrency}) ` + numberWithCommas(parseFloat(sum - count))  
             
         }
     })
     .catch(err=>console.log(err))
 }
 
+function modifyPrice(){
+    document.querySelector('tbody').addEventListener('click',function(e){
+       let grandtotal =  document.getElementById('grandtotal')
+       let discount =  document.getElementById('discount').value
+       
+        if(e.target.classList.contains('quantity')){
+            let price = e.target.parentElement.parentElement.children[3].children[0].value
+            let totalDiv = e.target.parentElement.parentElement.children[4].children[0]
+            e.target.addEventListener('change',function(e){
+                let total = e.target.value * price;
+                totalDiv.value = total
+                sumTotal(grandtotal,discount)
+                
+                
+            })
+        }
+
+        if(e.target.classList.contains('price')){
+            let quantity = e.target.parentElement.parentElement.children[2].children[0].value
+            let totalDiv = e.target.parentElement.parentElement.children[4].children[0]
+            e.target.addEventListener('change',function(e){
+                let total = e.target.value * quantity;
+                totalDiv.value = total
+                sumTotal(grandtotal,discount)
+                
+            })
+        }
+    })
+}
+
+function sumTotal(grandtotal,discount){
+    let x = document.querySelector('tbody').children;
+    let sum =0;
+    for(let i=0;i<x.length;i++){
+        console.log(x[i].children[4].children[0].value)
+        sum += parseFloat(x[i].children[4].children[0].value)
+    }
+    let d = parseFloat(discount/100);
+    
+    let getcurrency = currencySelect(currencyFetch);
+    grandtotal.innerHTML = ` (${getcurrency}) ` + numberWithCommas(sum - (d * sum)) 
+            
+}
+
+function modifyDiscount(){
+    
+    let discount =  document.getElementById('discount')
+    let grandtotal =  document.getElementById('grandtotal')
+    discount.addEventListener('change',function(e){
+        
+        let x = document.querySelector('tbody').children;
+        let sum = 0;
+        for(let i=0;i<x.length;i++){
+            console.log(x[i].children[4].children[0].value)
+            sum += parseFloat(x[i].children[4].children[0].value)
+        }
+        let d = parseFloat(e.target.value/100);
+        let getcurrency = currencySelect(currencyFetch);
+        grandtotal.innerHTML = ` (${getcurrency}) ` + numberWithCommas(sum - (d * sum)) 
+    })
+
+    
+}
+
+function currencySelect (currency){
+    switch(currency){
+        case 'NGN':
+            return '&#8358;'
+        case 'USD':
+            return '&#36;' 
+        case 'EURO':
+            return '&#x20AC;'
+        case 'GBP':
+            return '&#163;'
+        case 'YEN':
+        return '&#165;'
+    }
+}
 
