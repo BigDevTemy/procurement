5736e8cb45eadf32c865e5f96aeba9156add1fa4
TEMILOLUWA <bigdevtemy@gmail.com>
2023-01-02 21:26:36 +0100

    commit

--- app/customroute.php
+++ app/customroute.php
@@ -445,6 +445,13 @@ $router->post('savenewpo',function($request){
   $connection = new mysqli("localhost","root","","procurement");
   $data = json_decode(file_get_contents('php://input'), true);
   $newOrderref = createOrderRef($data['order_ref']);
+
+  if($data['poId']){
+    $query = "DELETE FROM po WHERE `id` ='".$data["poId"]."'";
+    $result = $connection->query($query)or die(mysqli_error($connection));
+    $queryII = "DELETE FROM items WHERE `po_id` ='".$data["poId"]."'";
+    $result = $connection->query($queryII)or die(mysqli_error($connection));
+  }
   
   $query = "INSERT INTO po (supplier_id,order_ref,delivery_days,delivery_address,body_note,shippment_type,new_order_ref,signatures)VALUES('".$data['supplier_id']."','".$data['order_ref']."','".$data['days']."','".$data['address']."','".$data['body']."','".$data['shippment_type']."','".$newOrderref."','".$data['signature']."')";
   $result = $connection->query($query)or die(mysqli_error($connection));
